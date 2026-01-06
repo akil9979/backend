b36be7a86d8f3aee93deec3fcc7f0e6270ea7f67
Akil Rizwani <akilrizwani627@gmail.com>
2026-01-06 13:21:07 +0530

    add user profile and watch history retrieval features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccesstoken} from "../controllers/user.controllers.js";
+import { loginUser,
+    logoutUser,
+     registerUser ,
+     refreshAccesstoken,
+     changeCurrentPassword,
+     getCurrentUser,
+     updateAccountDetails,
+     UpdateUserAvatar,
+     UpdateUserCoverImage,
+     getUserChannelProfile,
+     getWatchHistory,
+    } from "../controllers/user.controllers.js";
  import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -21,6 +32,15 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccesstoken)
+router.route("/change-Password").post(verifyJWT,changeCurrentPassword)
+router.route("/Current-User").post(verifyJWT,getCurrentUser)
+router.route("/update-account").post(verifyJWT,updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),UpdateUserAvatar)
+router.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage"),UpdateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 
